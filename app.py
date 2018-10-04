a1cb52ff64bdacc3830fce2c64d07f4b344c014d
catonsun <caton.sun@gmail.com>
2018-10-04 12:12:18 -0500

    refactored code

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
-
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+
+@app.route("/upload", methods=['POST', 'GET'])
 def upload():
     if request.method == 'POST':
         file = request.files['file']
         filename = file.filename
         file.save(os.path.join('UPLOAD_FOLDER', filename))
         return redirect(url_for('index'))
-    return render_template("home.html")
+    return render_template("upload.html")
 
 
 if __name__ == '__main__':

88d1f1a69e70fb33913bf765ab2df0f8f3599373
grace-fung <37249107+grace-fung@users.noreply.github.com>
2018-10-05 18:39:28 -0500

    Something pops up when we upload the image
    So I merged what I did during class. So now an image pops up when you press upload :/ but not on a self made UI... that's what the import tkinter stuff is for

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import tkinter as tk
+from PIL import Image, ImageTk
 app = Flask(__name__)
 
 
@@ -14,9 +16,14 @@ def upload():
         file = request.files['file']
         filename = file.filename
         file.save(os.path.join('UPLOAD_FOLDER', filename))
+        uploadAction(file)
         return redirect(url_for('index'))
     return render_template("upload.html")
 
+def uploadAction(filename):
+    picture = Image.open(filename)
+    picture.show()
+    return
 
 if __name__ == '__main__':
     app.run()

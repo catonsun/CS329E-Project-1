11047eaec3f28d708de4ddd6a8e4a6a07eead860
unknown <jameswu124@gmail.com>
2018-10-06 13:39:53 -0500

    new unit test test file...

---
+++ test.py
@@ -0,0 +1,36 @@
+import unittest
+
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import tkinter as tk
+from PIL import Image, ImageTk
+test = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+
+@app.route("/upload", methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = file.filename
+        return filename
+        return redirect(url_for('index'))
+    return render_template("upload.html")
+
+def documentType():
+    docType = filename[-3:]
+    return docType
+
+class Mytest(unittest.TestCase):
+    def isCorrectDocumentTypeLower(self):
+        self.assertEqual(docType.lower(), "jpg")
+
+    def isCorrectDocumentTypeUpper(self):
+        self.assertEqual(docType.upper(), "JPG")
+
+if __name__ == '__main__':
+    test.run()

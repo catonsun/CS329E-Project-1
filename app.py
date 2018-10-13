d72e97b9be5ce906e98763d70e689013064425eb
catonsun <caton.sun@gmail.com>
2018-10-13 18:58:27 -0500

    rerouted app to include edit page with relevant picture

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ filename = ""
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    if filename == "":
+        return render_template("home.html")
+    else:
+        return render_template("home2.html")
 
 
 @app.route("/upload", methods=['POST', 'GET'])
@@ -18,14 +21,22 @@ def upload():
     if request.method == 'POST':
         file = request.files['file']
         filename = file.filename
-        file.save(os.path.join('UPLOAD_FOLDER', filename))
+        setFileName(filename)
+        file.save(os.path.join('static', 'picture'))
         uploadAction(file)
         return redirect(url_for('index'))
     return render_template("upload.html")
 
+
 @app.route("/edit", methods=['POST', 'GET'])
 def edit():
-    return render_template("edit.html")
+    return render_template("edit.html", picture_name=filename)
+
+
+def setFileName(name):
+    global filename
+    filename = name
+
 
 def uploadAction(filename):
     picture = Image.open(filename)
@@ -39,6 +50,7 @@ def getFileName():
 
 def returnSuccess():
     return "success"
+
 
 if __name__ == '__main__':
     app.run()
